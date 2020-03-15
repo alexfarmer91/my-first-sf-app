a935684e8e6b11f4003423887c03c893e3a634ee
github username <github email address>
2020-03-15 10:31:42 -0400

    wiring components together

--- src/client/modules/my/sessionList/sessionList.js
+++ src/client/modules/my/sessionList/sessionList.js
@@ -4,8 +4,15 @@ export default class SessionList extends LightningElement {
     @track sessions = [];
     connectedCallback() {
         getSessions().then(result => {
-            console.log(result);
             this.sessions = this.allSessions = result;
         });
+    }
+
+    handleSessionClick(event) {
+        const index = event.currentTarget.dataset.index;
+        const navigateEvent = new CustomEvent('navigate', {
+            detail: this.sessions[index].id
+        });
+        this.dispatchEvent(navigateEvent);
     }
 }
